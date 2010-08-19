cb3560d3fbf27bb3773f5d126ea85847c859618b
kevin.mchale2 <kevin.mchale2@5f70aefe-f319-11dd-a8e2-f7cb3c35fcff>
2010-08-19 15:26:14 +0000

    UI updates

--- trunk/src/ui.h
+++ trunk/src/ui.h
@@ -1,23 +1,32 @@
+/*
+ * UI.h - User Interface header
+ */
+#ifndef UI_H
+#define UI_H
+ 
+typedef struct {
+	unsigned cmd:4;
+	unsigned ops:3;
+	unsigned su:1;
+} command;
 
 #define uiSU_REQD		0x10	// bit 5 indicates SU must be enabled
 #define uiBUFFER_SIZE	30
 
 #define cmdSTATUS		0x01
 #define cmdRCVMSGS		0x02
-#define cmdCLOSE_RELAY	0x11
-#define cmdOPEN_RELAY	0x12
 #define cmdRED_ON		0x03
 #define cmdRED_OFF		0x04
 #define cmdGREEN_ON		0x05
 #define cmdGREEN_OFF	0x06
-#define cmdGET_VALUE	0x07
-#define cmdSU			0x08
-#define cmdSU_OFF		0x13
-#define cmdVOLTS		0x09
-#define cmdCURRENT		0x0A
-#define cmdTEMP			0x0B
-#define cmdMSGS			0x0C
+#define cmdGET_VALUE	0x47
+#define cmdCONV_RAW		0x08
+#define cmdCONV_FLOAT	0x09
 
+#define cmdSU			0x09
+#define cmdCLOSE_RELAY	0x11
+#define cmdOPEN_RELAY	0x12
+#define cmdSU_OFF		0x13
 
 const unsigned char txtSTATUS[] = 		"status";
 const unsigned char txtRCVMSGS[] =		"rmsgs";
@@ -33,6 +42,16 @@ const unsigned char txtSU_OFF[] =		"nosu";
 const unsigned char txtVOLTS[] = 		"volts";
 const unsigned char txtCURRENT[] =		"current";
 const unsigned char txtMSGS[] =			"msgs";
+const unsigned char txtCONV_RAW[] = 	"convr";
+const unsigned char txtCONV_FLOAT[] =	"convf";
 const unsigned char txtCRLF[] = 		"\r\n";
 
-// Errors
+// UI States
+#define uiState_RESET			0x00
+#define uiState_SYSTEM_PROMPT	0x01
+#define uiState_WAIT_FOR_CMD	0x02
+#define uiState_PROCESS_CMD		0x03
+#define uiState_WAIT_FOR_INPUT1	0x04
+#define uiState_WAIT_FOR_INPUT2 0x05
+
+#endif /* UI_H */
